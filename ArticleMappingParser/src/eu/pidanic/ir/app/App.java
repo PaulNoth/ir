2cecc9ce3ab01b0996cc1d37a13c45b2682c3b60
Paul P <paulp@hotmail.sk>
2014-10-05 00:02:10 +0200

    first implementation of interlanguage links parser

--- ArticleMappingParser/src/eu/pidanic/ir/app/App.java
+++ ArticleMappingParser/src/eu/pidanic/ir/app/App.java
@@ -1,5 +1,15 @@
 package eu.pidanic.ir.app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import eu.pidanic.ir.util.Language;
+
 /**
  * 
  * Main class.
@@ -9,8 +19,95 @@ package eu.pidanic.ir.app;
  */
 public class App
 {
-	public static void main(String[] args)
-	{
 
+	private static final List<String> LOCAL_FILES;
+	static
+	{
+		List<String> temp = new ArrayList<>();
+		// temp.add("C:\\Users\\Paul P\\Desktop\\New folder\\wikipedia_links_en.ttl");
+		temp.add("C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_en.ttl");
+		// temp.add("C:\\Users\\Paul P\\Desktop\\New folder\\wikipedia_links_de.ttl");
+		temp.add("C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_de.ttl");
+		// temp.add("C:\\Users\\Paul P\\Desktop\\New folder\\wikipedia_links_sk.ttl");
+		temp.add("C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_sk.ttl");
+		temp.add("C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_fr.ttl");
+		LOCAL_FILES = Collections.unmodifiableList(temp);
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		File dir = new File(
+		// "C:\\Users\\Paul P\\Desktop\\New folder\\wikipedia_links_en.ttl");
+		// "C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_en.ttl");
+		// "C:\\Users\\Paul P\\Desktop\\New folder\\wikipedia_links_de.ttl");
+		// "C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_de.ttl");
+		// "C:\\Users\\Paul P\\Desktop\\New folder\\wikipedia_links_sk.ttl");
+		// "C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_sk.ttl");
+		// "C:\\Users\\Paul P\\Desktop\\New folder\\interlanguage_links_fr.ttl");
+		// "data" + File.separator + "sample_interlanguage_links_sk.ttl");
+				"data" + File.separator + "interlanguage_links");
+		for (File f : dir.listFiles())
+		// for (String localPath : LOCAL_FILES)
+		{
+			// BufferedReader br = new BufferedReader(new FileReader(f));
+			// File f = new File(localPath);
+			BufferedReader br = new BufferedReader(new FileReader(f));
+
+			String line = null;
+			String lastResource = "";
+			List<Resource> result = new ArrayList<>();
+			while ((line = br.readLine()) != null)
+			{
+				// System.out.println(line);
+				if (!line.startsWith("#"))
+				{
+					String[] resources = line.split("\\s+");
+					String resource = removeBracket(resources[0]);
+					String translResource = removeBracket(resources[2]);
+
+					if (!lastResource.equals(resource))
+					{
+						String id = parseWord(translResource);
+						Language searchLang = Language
+								.valueOf(f
+										.getName()
+										.substring(
+												f.getName().lastIndexOf("_") + 1,
+												f.getName().indexOf("."))
+										.toUpperCase());
+						Resource r = new Resource(id, searchLang, resource);
+						System.out.println(r);
+						// result.add(r);
+						lastResource = resource;
+					}
+				}
+			}
+			for (Resource r : result)
+			{
+				System.out.println(r);
+			}
+			br.close();
+		}
+
+	}
+
+	private static void removeBrackets(String[] array)
+	{
+		for (int i = 0; i < array.length; i++)
+		{
+			array[i] = array[i].substring(1, array[i].length() - 1);
+		}
+	}
+
+	private static String removeBracket(String link)
+	{
+		return link.substring(1, link.length() - 1);
+	}
+
+	private static String parseWord(String link)
+	{
+		int lastSlash = link.lastIndexOf("/");
+		String word = link.substring(lastSlash + 1, link.length());
+		return word;
 	}
 }
---
+++ ArticleMappingParser/src/eu/pidanic/ir/app/Resource.java
@@ -0,0 +1,45 @@
+package eu.pidanic.ir.app;
+
+import eu.pidanic.ir.util.Language;
+
+/**
+ * 
+ * @author Pidanic
+ *
+ */
+public final class Resource
+{
+	private final String id;
+
+	private final String url;
+
+	private final Language lang;
+
+	public Resource(String id, Language lang, String url)
+	{
+		this.lang = lang;
+		this.id = id;
+		this.url = url;
+	}
+
+	public Language getLanguage()
+	{
+		return lang;
+	}
+
+	public String getUrl()
+	{
+		return url;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	@Override
+	public String toString()
+	{
+		return id + " | " + lang + " | " + url;
+	}
+}
